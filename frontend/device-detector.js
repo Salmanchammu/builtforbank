5b31ffee6d066e24aacc1759256ca76d71728607
Salman <salman@gmail.com>
2026-03-12 00:59:48 +0530

    feat: Implement automated device redirection between Desktop and Mobile

--- frontend/device-detector.js
+++ frontend/device-detector.js
@@ -4,6 +4,23 @@
  */
 (function () {
     window.SmartBankDeviceDetector = {
+        // Redirection Mapping: [Desktop Page] -> [Mobile Page]
+        redirectionMap: {
+            'index.html': 'mobile-auth.html',
+            'user.html': 'mobile-auth.html',
+            'signup.html': 'mobile-signup.html',
+            'forgot-password.html': 'mobile-forgot-password.html',
+            'userdash.html': 'mobile-dash.html'
+        },
+
+        // Reverse Mapping: [Mobile Page] -> [Desktop Page]
+        reverseMap: {
+            'mobile-auth.html': 'user.html',
+            'mobile-signup.html': 'signup.html',
+            'mobile-forgot-password.html': 'forgot-password.html',
+            'mobile-dash.html': 'userdash.html'
+        },
+
         getDeviceType: function () {
             const ua = navigator.userAgent;
             if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
@@ -15,16 +32,32 @@
             return "desktop";
         },
 
-        // Helper to get formatted device info for logging
-        getDeviceInfo: function () {
-            return {
-                type: this.getDeviceType(),
-                platform: navigator.platform,
-                userAgent: navigator.userAgent,
-                screen: `${window.screen.width}x${window.screen.height}`
-            };
+        checkAndRedirect: function () {
+            const deviceType = this.getDeviceType();
+            const currentPath = window.location.pathname.split('/').pop() || 'index.html';
+            
+            // If on mobile device and current page has a mobile equivalent
+            if (deviceType === 'mobile' || deviceType === 'tablet') {
+                if (this.redirectionMap[currentPath]) {
+                    console.log(`Mobile detected. Redirecting ${currentPath} -> ${this.redirectionMap[currentPath]}`);
+                    window.location.href = this.redirectionMap[currentPath];
+                }
+            } 
+            // If on desktop device and current page is mobile-only
+            else if (deviceType === 'desktop') {
+                if (this.reverseMap[currentPath]) {
+                    console.log(`Desktop detected. Redirecting ${currentPath} -> ${this.reverseMap[currentPath]}`);
+                    window.location.href = this.reverseMap[currentPath];
+                }
+            }
         }
     };
+
+    // Auto-execute if script is loaded with 'data-auto-redirect' attribute
+    const currentScript = document.currentScript;
+    if (currentScript && currentScript.hasAttribute('data-auto-redirect')) {
+        window.SmartBankDeviceDetector.checkAndRedirect();
+    }
 
     console.log("Device detector initialized: ", window.SmartBankDeviceDetector.getDeviceType());
 })();
